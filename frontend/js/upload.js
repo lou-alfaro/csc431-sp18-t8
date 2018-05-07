5db83068222b85e758f7046675d37bf98d1f1f1a
deeyago <jugo415@gmail.com>
2018-05-07 00:53:47 -0400

    added upload functionality to AWS
    files submitted through a form

---
+++ frontend/js/upload.js
@@ -0,0 +1,38 @@
+var albumBucketName = 'csc431-sp18-workshop-maps';
+var bucketRegion = 'us-east';
+var IdentityPoolId = '2:4b4f3a05-fde4-430e-a530-a004d031d703';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
+
+function addPhoto(albumName) {
+  var files = document.getElementById('photoupload').files;
+  if (!files.length) {
+    return alert('Please choose a file to upload first.');
+  }
+  var file = files[0];
+  var fileName = file.name;
+  var albumPhotosKey = encodeURIComponent(albumName) + '//';
+
+  var photoKey = albumPhotosKey + fileName;
+  s3.upload({
+    Key: photoKey,
+    Body: file,
+    ACL: 'public-read'
+  }, function(err, data) {
+    if (err) {
+      return alert('There was an error uploading your photo: ', err.message);
+    }
+    alert('Successfully uploaded photo.');
+    viewAlbum(albumName);
+  });
+}
